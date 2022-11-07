64f748a12fe01dacd78ec2fe66c09b720fc42117
Gael Mukendi Kabongo <111666680+gmka98@users.noreply.github.com>
2022-11-07 16:31:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+//create the "ul" and "li" tag and connect them  
+const body = document.querySelector("body")
+const Ul = document.createElement("ul")
+const Id = document.createElement
+const Li = document.createElement("li")
+const union = Ul.appendChild(Li)
+ body.appendChild(Ul)
+//Create a new list item when clicking on the "Add" button
+
+function newTask(){
+    const Ul = document.querySelector("ul");Ul.setAttribute('#myUl');
+    const input = document.getElementById("myInput").value
+    const txt = document.createTextNode(inputValue)
+    list.appendChild(txt)
+
+    if(inputValue === ''){
+        alert("Enter some task name! young JOHNSON!!")
+    }
+    
+    else{
+       document.getElementById('myInput').value = ""
+}
+
+
+const span = document.createElement("SPAN")
+const textNode = document.createTextNode("\u00D7")
+span.className = "close";
+span.appendChild(textNode);
+NodeISY[z].appendChild(span);
+
+
+for (i = 0; i < close.length; i++) {
+    close[z].onclick = function(){
+        const div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+}
+
+//create a "close" button and append it to each list item
+const NodeISY = document.querySelector("li")
+var z;
+
+for (z = 0; z < NodeISY.lenght; z++) {
+const span = document.createElement("SPAN")
+const textNode = document.createTextNode("\u00D7")
+span.className = "close";
+span.appendChild(textNode);
+NodeISY[z].appendChild(span)
+
+}
